e1b731d7e2113242a5aa6791232dc4f5bd4c56ba
superbeom <it.super.beom@gmail.com>
2019-04-30 10:24:18 +0900

    #3.3 sendMail Function with Nodemailer

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,37 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
 
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+
+  const client = nodemailer.createTransport(sgTransport(options));
+
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "super@prismagram.com",
+    to: address,
+    subject: "🔒 Login Secret for Prismagram 🔑",
+    html: `Hello! Your login secret is '${secret}'.<br/>Copy paste on the app/website to log in!`
+  };
+
+  return sendMail(email);
 };
