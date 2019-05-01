d350abf226f9d8de96cf5c5af72d69ac460d3e22
superbeom <it.super.beom@gmail.com>
2019-05-01 15:17:38 +0900

    #3.6 Passport JWT part Three

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,11 +3,13 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args) => {
+    requestSecret: async (_, args, { request }) => {
+      // console.log(request);
       const { email } = args;
       const loginSecret = generateSecret();
       console.log(loginSecret);
       try {
+        throw Error("testing");
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
