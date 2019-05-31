c92450791096ab1481287a860febe00371a0f3de
superbeom <it.super.beom@gmail.com>
2019-05-31 16:05:03 +0900

    [Frontend] #5.6 createAccount Mutation part Two

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,11 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ email });
+      if (exists) {
+        throw Error("This email is already taken");
+      }
+
       await prisma.createUser({
         username,
         email,
